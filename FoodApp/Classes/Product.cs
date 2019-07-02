4d85a52d6396f78774478ebe8c30c0328bf3666d
Maksym Lastovskyi <lastmax.job@gmail.com>
2019-07-02 14:12:44 +0300

    Добавил колецию Storage, класс Product

---
+++ FoodApp/Classes/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodApp
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Section { get; set; }
+        public double Price { get; set; }
+        public int Weight { get; set; }
+
+        public Product(string name, string section, double price, int weight)
+        {
+            Name = name;
+            Section = section;
+            Price = price;
+            Weight = weight;
+        }
+
+        public int CheckOnStorage(Storage storage)
+        {
+            return storage.ShowQuantity(this);
+        }
+    }
+}
---
+++ FoodApp/Collections/Storage.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace FoodApp
+{
+    [Serializable]
+    public class Storage : IEnumerable, IEnumerator
+    {
+        Dictionary<Product, int> storage = new Dictionary<Product, int>();
+
+        public int ShowQuantity(Product product)
+        {
+            return storage[product];
+        }
+
+        public void Remove(Product product, int quantity)
+        {
+            storage[product] -= quantity;
+        }
+
+        public void Add(Product product, int quantity)
+        {
+            storage[product] += quantity;
+        }
+
+        int position = -1;
+
+        public object Current
+        {
+            get { return storage.ElementAt(position); }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            if(position < storage.Count - 1)
+            {
+                position++;
+                return true;
+            }
+            else
+            {
+                Reset();
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public Storage Load()
+        {
+            Storage loadStorage;
+
+            if (File.Exists("storage.dat"))
+            {
+                FileStream usersFileStream = new FileStream("storage.dat", FileMode.Open, FileAccess.ReadWrite);
+                BinaryFormatter formatter = new BinaryFormatter();
+                loadStorage = formatter.Deserialize(usersFileStream) as Storage;
+                usersFileStream.Close();
+            }
+            else
+            {
+                loadStorage = new Storage();
+            }
+
+            return loadStorage;
+        }
+
+        public void Save(Storage storage)
+        {
+            FileStream stream = new FileStream("storage.dat", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, storage);
+            stream.Close();
+        }
+    }
+}
