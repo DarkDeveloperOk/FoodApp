2a9541604c78317775ca5b7fba6c6214662a19c7
Maksym Lastovskyi <lastmax.job@gmail.com>
2019-07-07 21:34:11 +0300

    сделал ShoppingCart

--- FoodApp/Classes/ShoppingCart.cs
+++ FoodApp/Classes/ShoppingCart.cs
@@ -9,26 +9,41 @@ namespace FoodApp
     class ShoppingCart
     {
         Dictionary<Product, int> products = new Dictionary<Product, int>();
-        double sum = 0;
 
         public void Add(Product product, int quantity)
         {
-            products.Add(product, quantity);
-            sum += (product.Price * quantity);
+            if (products.ContainsKey(product))
+            {
+                products[product] += quantity;
+            }
+            else
+            {
+                products.Add(product, quantity);
+            }
         }
 
         public void Subtract(Product product, int quantity)
         {
-            if(quantity >= products[product])
+            if (quantity >= products[product])
             {
                 products.Remove(product);
-                sum -= product.Price * products[product];
             }
             else
             {
                 products[product] -= quantity;
-                sum -= (product.Price * quantity);
             }
+        }
+
+        public double GetSum()
+        {
+            double sum = 0;
+
+            foreach (KeyValuePair<Product, int> pair in products)
+            {
+                sum += (pair.Key.Price * pair.Value);
+            }
+
+            return sum;
         }
 
         public void ShowCart()
@@ -42,13 +57,138 @@ namespace FoodApp
 
             foreach (KeyValuePair<Product, int> pair in products)
             {
+                int number = 0;
                 string name = pair.Key.Name.PadRight(pair.Key.Name.Length + (25 - pair.Key.Name.Length));
 
-                Console.WriteLine("{0} {1, -11} {2, -4} {3}", name, pair.Key.Price, pair.Value, pair.Key.Price * pair.Value);
+                Console.WriteLine("{0}.{1} {2, -11} {3, -4} {4}", number += 1, name, pair.Key.Price, pair.Value, pair.Key.Price * pair.Value);
             }
             Console.WriteLine(new string('-', 50));
-            Console.WriteLine("Общая сумма заказа: {0, 29}", sum);
+            Console.WriteLine("Общая сумма заказа: {0, 29}" + Environment.NewLine, GetSum());
         }
 
+        private void EditShoppingCart()
+        {
+            Console.Write("Номер товара, который надо изменить: ");
+            Product product;
+
+            while (true)
+            {
+                int productNumber;
+                string prod = Console.ReadLine();
+                bool parseResult = int.TryParse(prod, out productNumber);
+
+                if (!parseResult || productNumber < 1 || productNumber > products.Count)
+                {
+                    Console.Write("Не допустимый ввод, повторите порытку: ");
+                    continue;
+                }
+
+                product = products.ElementAt(productNumber - 1).Key;
+                break;
+            }
+
+
+            Console.WriteLine("1. Добавить 2. Убрать 3. Отмена");
+            string choice;
+
+            while (true)
+            {
+                choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    int quantity;
+
+                    Console.Write("Количество: ");
+                    while(true)
+                    {
+                        string quant = Console.ReadLine();
+                        bool parseResult = int.TryParse(quant, out quantity);
+
+                        if (!parseResult || quantity < 0)
+                        {
+                            Console.Write("Не допустимый ввод, повторите порытку: ");
+                            continue;
+                        }
+                        break;
+                    }
+
+                    Add(product, quantity);
+                }
+
+                else if(choice == "2")
+                {
+                    int quantity;
+
+                    Console.Write("Количество: ");
+                    while (true)
+                    {
+                        string quant = Console.ReadLine();
+                        bool parseResult = int.TryParse(quant, out quantity);
+
+                        if (!parseResult || quantity < 0)
+                        {
+                            Console.Write("Не допустимый ввод, повторите порытку: ");
+                            continue;
+                        }
+                        break;
+                    }
+
+                    Subtract(product, quantity);
+                }
+
+                else if(choice == "3")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write("Не допустимый ввод, повторите порытку: ");
+                    continue;
+                }
+                break;
+            }
+        }
+
+        public int doChoice()
+        {
+            Console.WriteLine("cont - продолжить заказивать; edit - редактировать; send - отправить; quit - выход;");
+            string choose;
+            int endCode;
+
+            while (true)
+            {
+                choose = Console.ReadLine().ToLower();
+
+                if (choose == "quit")
+                {
+                    Environment.Exit(0);
+                }
+                else if (choose == "cont")
+                {
+                    endCode = 1;
+                    break;
+                }
+                else if (choose == "edit")
+                {
+                    EditShoppingCart();
+                    Console.Clear();
+                    ShowCart();
+                    continue;
+                }
+                else if (choose == "send")
+                {
+                    endCode = -1;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Нет такой команды, повторите ввод");
+                    continue;
+                }
+            }
+
+            return endCode;
+        }
     }
 }
